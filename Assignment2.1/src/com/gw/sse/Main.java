3565a9a6ce3360cecd93b21d7d268e9888eb80f1
GWhitehurst <90222043+GWhitehurst@users.noreply.github.com>
2021-09-12 17:41:16 -0700

    updated to use command line

--- Assignment2.1/src/com/gw/sse/Main.java
+++ Assignment2.1/src/com/gw/sse/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
     public static void main(String [] args) {
         double total = 0;
-        String[] name = {"-1", "Hello", "2.5", "3", "4.9", "10"};
-        for(int i = 0; i < name.length; i++){
+        for(int i = 0; i < args.length; i++){
             try{
-                total = total + Double.parseDouble(name[i]);
+                total = total + Double.parseDouble(args[i]);
             }
             catch(Exception e){
                 System.out.println("An improper number was entered and ignored.");
